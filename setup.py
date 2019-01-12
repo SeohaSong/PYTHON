7f5c525dcd8d9bb9b81de03ded61e34fe729c8ec
seohasong <tisutoo@gmail.com>
2019-01-13 02:10:51 +0900

    update origin master

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="SEOHASONG Packages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/SeohaSong/python-shs",
+    url="https://github.com/SeohaSong/python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
