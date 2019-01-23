8486365262fa6cc11d15829565c76c180a90bce3
seohasong <tisutoo@gmail.com>
2019-01-23 19:50:30 +0900

    continue previous work

--- shs/tailor/__init__.py
+++ shs/tailor/__init__.py
@@ -1,2 +1,2 @@
 
-from .init import calc_f1_score
+from .init import Score
--- shs/tailor/init.py
+++ shs/tailor/init.py
@@ -2,13 +2,38 @@
 import numpy as np
 
 
-def calc_f1_score(trues, preds):
-    trues = np.array(trues)*1
-    preds = np.array(preds)*1
-    p1, = np.where(preds == 1)
-    t1, = np.where(trues == 1)
-    tp = np.intersect1d(p1, t1)
-    p = len(tp)/len(p1)
-    r = len(tp)/len(t1)
-    score = (2*p*r)/(p+r)
-    return score
+class Score():
+
+    def __init__(self, trues, preds, reverse=False):
+        trues = np.array(trues)*1
+        preds = np.array(preds)*1
+        if reverse:
+            trues = 1-np.array(trues)
+            preds = 1-np.array(preds)
+        p1, = np.where(preds == 1)
+        t1, = np.where(trues == 1)
+        tp = np.intersect1d(p1, t1)
+        self._p1 = p1
+        self._t1 = t1
+        self._tp = tp
+
+
+    def calc_f1(self):
+        p = self.calc_precision()
+        r = self.calc_recall()
+        score = 2/(1/p+1/r)
+        return score
+
+
+    def calc_precision(self):
+        p1 = self._p1
+        tp = self._tp
+        score = len(tp)/len(p1)
+        return score
+
+
+    def calc_recall(self):
+        t1 = self._t1
+        tp = self._tp
+        score = len(tp)/len(t1)
+        return score
