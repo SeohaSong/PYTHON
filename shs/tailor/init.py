77d087a5f8c864260225ef7701d78df1c82c6582
seohasong <tisutoo@gmail.com>
2019-02-20 02:27:34 +0900

    continue previous work

--- shs/tailor/init.py
+++ shs/tailor/init.py
@@ -10,8 +10,8 @@ class Score():
         if reverse:
             trues = 1-np.array(trues)
             preds = 1-np.array(preds)
-        p1, = np.where(preds == 1)
-        t1, = np.where(trues == 1)
+        p1 = np.where(preds == 1)[0]
+        t1 = np.where(trues == 1)[0]
         tp = np.intersect1d(p1, t1)
         self._p1 = p1
         self._t1 = t1
